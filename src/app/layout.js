3c2d217099ba5aca712efce02cf40ca9d9b454cd
Miguel López Ulloa <mikauran@hotmail.com>
2025-04-23 00:40:00 -0600

    Adding the new font for the Artist default (#4)
    Ok.  The next couple day.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { inter } from './fonts';
 import "@/app/styles/globals.css";
 import Head from "next/head";
 import Navbar from "@/components/Navbar";
@@ -7,8 +7,6 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import Copyright from "@/components/Copyright";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Anita Gonzalez Gallery",
@@ -19,33 +17,32 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
   return (
-      <ThemeProvider>
-        <Head>
-            <Seo />
-        </Head>
+    <ThemeProvider>
+      <Head>
+        <Seo />
+      </Head>
 
-        <html lang="en">
-          <body
-                      className={`${inter.className} bg-gray-50 dark:bg-gray-800 transition-colors 
+      <html lang="en">
+        <body
+          className={`${inter.className} bg-gray-50 dark:bg-gray-800 transition-colors 
                       duration-300 grid grid-rows-[auto_1fr_auto] min-h-screen`}
-          >
+        >
+          {/* Navbar */}
+          <Navbar />
 
-            {/* Navbar */}
-            <Navbar/>
+          {/* Main Content */}
+          <main className="flex-grow w-full overflow-x-hidden">
+            {children}
+            <ToastContainer />
+          </main>
 
-            {/* Main Content */}
-            <main className="flex-grow w-full overflow-x-hidden">{children}
-              <ToastContainer />
-            </main>
-
-            {/* Footer */}
-            <footer className="w-full bg-gray-100 dark:bg-gray-900 py-6 px-4 sm:px-6 lg:px-8">
-              <Copyright />
-            </footer>
-          </body>
-        </html>
-      </ThemeProvider>
+          {/* Footer */}
+          <footer className="w-full bg-gray-100 dark:bg-gray-900 py-6 px-4 sm:px-6 lg:px-8">
+            <Copyright />
+          </footer>
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
